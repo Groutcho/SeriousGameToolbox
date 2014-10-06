4458341730e8d5c817e714ea7fc79cb19b67cb86
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-10-06 20:56:06 +0200

    Initial commit

---
+++ SeriousGameToolbox/SeriousGameToolbox/Pedagogy/Session.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SeriousGameToolbox.Pedagogy
+{
+    public class Session
+    {
+        protected DateTime date = DateTime.Now;
+        protected TimeSpan duration = new TimeSpan(0);
+
+        public DateTime Date { get { return date; } }
+        public TimeSpan Duration { get { return duration; } }
+
+        private DateTime startTime = DateTime.Now;
+        private DateTime endTime = DateTime.Now;
+
+        public virtual void Start()
+        {
+            date = DateTime.Today;
+            startTime = DateTime.Now;
+        }
+
+        public virtual void End()
+        {
+            endTime = DateTime.Now;
+            duration = endTime - startTime;
+        }
+
+        public Session()
+        {
+
+        }
+    }
+}
