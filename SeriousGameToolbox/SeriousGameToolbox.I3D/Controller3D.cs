cb86f7506225437dd03d059805943a969e909964
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-10-10 23:48:16 +0200

    Added SeriousGameToolbox.I3D stub

---
+++ SeriousGameToolbox/SeriousGameToolbox.I3D/Controller3D.cs
@@ -0,0 +1,16 @@
+﻿using SeriousGameToolbox.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeriousGameToolbox.I3D
+{
+    public class Controller3D : IUpdatable
+    {
+        public void Update(double dt)
+        {
+            
+        }
+    }
+}
