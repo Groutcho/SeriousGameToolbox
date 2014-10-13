b44de2ba2d375a74c4ede1eed8fe7eeb1c359a3a
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-10-13 04:17:47 +0200

    Added FileLoggerChannel

---
+++ SeriousGameToolbox/SeriousGameToolbox/Logging/FileLoggerChannel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SeriousGameToolbox.Logging
+{
+    public class FileLoggerChannel : ILoggerChannel
+    {
+        StreamWriter writer;
+
+        public FileLoggerChannel(string filename)
+        {
+            writer = new StreamWriter(filename);
+            writer.AutoFlush = true;
+        }
+
+        public void Log(object message, EntryGravity gravity)
+        {
+            writer.WriteLine(string.Format("{0};{1};{2}", gravity, message, DateTime.Now.ToString()));
+        }
+
+        public void Dispose()
+        {
+            writer.Flush();
+            writer.Dispose();
+        }
+    }
+}
