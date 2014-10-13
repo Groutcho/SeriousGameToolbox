4233a996677b849bf6f5d784be0b4854fc62dfaa
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-10-13 17:51:21 +0200

    Added blinking capabilities to FrameDecorator

--- SeriousGameToolbox/SeriousGameToolbox.View/Decorators/FrameDecorator.cs
+++ SeriousGameToolbox/SeriousGameToolbox.View/Decorators/FrameDecorator.cs
@@ -9,14 +9,28 @@ namespace SeriousGameToolbox.I2D.Decorators
 {
     public class FrameDecorator : Decorator
     {
+        public bool Blink { get; set; }
+
+        float f;
+
         public FrameDecorator(Area area)
             : base(area)
         {
+            f = 0;
         }
 
         public override void Draw()
         {
             base.Draw();
+
+            if (Blink)
+            {
+                f += Time.deltaTime * 4;
+                var c = GUI.color;
+                c.a = (Mathf.Sin(f) + 1) / 2;
+                GUI.color = c;
+            }
+
             GUI.Box(Area, GUIContent.none, Style);
         }
     }
