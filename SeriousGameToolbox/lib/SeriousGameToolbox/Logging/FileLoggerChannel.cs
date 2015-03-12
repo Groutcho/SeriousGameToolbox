cc7d2b42b3c0c5ee6588b3bef7d19909a8382b94
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-03-12 22:00:48 +0100

    deleted blank line

--- SeriousGameToolbox/lib/SeriousGameToolbox/Logging/FileLoggerChannel.cs
+++ SeriousGameToolbox/lib/SeriousGameToolbox/Logging/FileLoggerChannel.cs
@@ -12,7 +12,6 @@ namespace SeriousGameToolbox.Logging
     {
         StreamWriter writer;
 
-
         public FileLoggerChannel(string filename)
         {
             writer = new StreamWriter(filename);
