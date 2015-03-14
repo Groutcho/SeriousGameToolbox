f5d81de88c2ccd76ed4cbaf1864c29e5dd161374
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-03-14 20:21:04 +0100

    Updated coverity creation batch to remove previous output

--- SeriousGameToolbox/coverity/create_coverity_package.bat
+++ SeriousGameToolbox/coverity/create_coverity_package.bat
@@ -1,4 +1,7 @@
 rem will create a coverity cov-int package ready to send to analysis
+rmdir cov-int /s
+del cov-int.zip
+
 set cov_path=%~dp0
 set sln_path=%~dp0..\
 cov-build --dir %cov_path%cov-int devenv %sln_path%SeriousGameToolbox.sln /rebuild Release
