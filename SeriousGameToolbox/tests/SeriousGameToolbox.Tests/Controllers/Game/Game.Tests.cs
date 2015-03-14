32796a44e091fe19008ebc96e71adb65af8bcac4
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-03-14 21:03:11 +0100

    added tests for Game

---
+++ SeriousGameToolbox/tests/SeriousGameToolbox.Tests/Controllers/Game/Game.Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using SeriousGameToolbox.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeriousGameToolbox.Tests.Controllers.Game_Test
+{
+    [TestFixture]
+    public class Game_Tests
+    {
+        public class GameMock : Game
+        {
+            public double ElapsedTime
+            {
+                get { return timeSinceStartInSeconds; }
+            }
+
+            public void TriggerStart()
+            {
+                Start();
+            }
+        }
+
+        [Test]
+        [Category("Game")]
+        public void ElapsedTimeIsCorrect()
+        {
+            GameMock mock = new GameMock();
+            mock.Update(122d);
+
+            Assert.AreEqual(122d, mock.ElapsedTime, 0.01d);
+        }
+
+        [Test]
+        [Category("Game")]
+        [ExpectedException(typeof(ApplicationException))]
+        public void StartThrowsApplicationExceptionWhenNoPhases()
+        {
+            GameMock mock = new GameMock();
+            mock.TriggerStart();
+        }
+    }
+}
